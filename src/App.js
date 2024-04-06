601aeb1008f758351703eea98c54ed53f5ff575f
banya <bjgeumgang@gmail.com>
2024-04-06 22:22:00 +0900

    util, env파일추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage/Homepage";
 import MoviePage from "./pages/Movies/MoviePage";
 import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 /* 
 3.Route 구성하기
@@ -27,11 +28,11 @@ function App() {
       <Route path="/" element={<AppLayout />}>
         <Route index element={<Homepage />} />
         <Route path="movies">
-          <Route index element={MoviePage} />
-          <Route path=":id" element={MovieDetailPage} />
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
         </Route>
       </Route>
-      <Route path="*" element={NotFoundPage} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -1,7 +1,56 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Outlet } from "react-router-dom"; // 라우터 안에있는 자손들을 가지고 오게 도와주는 컴포넌트
 
 const AppLayout = () => {
-  return <div>navbar</div>;
+  return (
+    <div>
+      <Navbar
+        expand="lg"
+        className="bg-body-dark"
+        // bg="dark"
+        data-bs-theme="dark"
+      >
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img
+              src="https://images.ctfassets.net/4cd45et68cgf/4nBnsuPq03diC5eHXnQYx/d48a4664cdc48b6065b0be2d0c7bc388/Netflix-Logo.jpg"
+              width={70}
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link href="/" className="link-light">
+                Home
+              </Nav.Link>
+              <Nav.Link href="/movies" className="link-light">
+                Movies
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
 };
 
 export default AppLayout;
