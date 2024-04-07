0e9da5b77c5a9f49634610784eb933754d8dd04e
banya <bjgeumgang@gmail.com>
2024-04-07 21:54:56 +0900

    feat: Benner, 리액트쿼리 관련 파일 추가

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Banner from "./components/Banner/Banner";
+
+/* 
+1. 배너만들기 => popular 영화를 들고와서 첫번째 아이템을 보여주자
+2. popular movie 가져오기
+3. top rated movie 가져오기
+4. upcoming movie 가져오기
+*/
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Homepage;
